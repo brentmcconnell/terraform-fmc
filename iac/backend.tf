9585256a2a4e6d5dba5ebe6658631b3b6de0691b
Brent McConnell <brmcconn@microsoft.com>
2021-01-18 16:06:10 -0500

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = ">=0.12.6"
   backend "azurerm" {
-    skip_provider_registration = true
     storage_account_name    = "tfstateacct0693"
     container_name          = "tfcont-0693"
     key                     = "terraform.tfstate"
