d1bb61978166431eab95d791e48cd22e7b55fb74
Brent McConnell <brmcconn@microsoft.com>
2021-01-18 16:06:31 -0500

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   version                   = ">=2.0.0"
+  skip_provider_registration = true
   client_id                 = "${var.clientId}"
   client_certificate_path   = "../service-principal.pfx"
   subscription_id           = "${var.subscriptionId}"
