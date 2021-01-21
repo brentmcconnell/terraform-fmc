7bd838cf7c918371b5a168d08995fa56407bef7d
brentmcconnell <emconne@gmail.com>
2021-01-21 10:05:36 -0500

    job run changes

--- iac/main.tf
+++ iac/main.tf
@@ -92,7 +92,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                              = "${local.prefix}-vm"
   location                          = local.location 
   resource_group_name               = data.azurerm_resource_group.project-rg.name 
-  vm_size                           = "Standard_DS12_v2"
+  vm_size                           = "Standard_D96as_v4"
 
   storage_image_reference {
     id = data.azurerm_image.fmc-img.id 
