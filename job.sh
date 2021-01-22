b05e85d2e5aece159951ee104a34368b9cb7dd0e
brentmcconnell <emconne@gmail.com>
2021-01-21 19:54:47 -0500

    added cloud-init dir

--- job.sh
+++ job.sh
@@ -3,6 +3,13 @@
 conda create -n denovo_asm
 conda activate denovo_asm
 cd /data/input
-wget https://sra-download.ncbi.nlm.nih.gov/traces/dra4/DRR/000208/DRR213641
+wget --progress=bar:force:noscroll https://sra-download.ncbi.nlm.nih.gov/traces/dra4/DRR/000208/DRR213641
+if ! [ -f ./DRR213641 ]; then
+  rm ./DRR213641
+fi
+
+if ! [ -f ./DRR213641.fastq ]; then
+  rm ./DRR213641.fastq
+fi
 fasterq-dump ./DRR213641
 time canu -p RKN -d /data/assembly/RKN_canu genomeSize=0.2g corMhapFilterThreshold=0.0000000002 corMhapOptions="--threshold 0.80 --num-hashes 512 --num-min-matches 3 --ordered-sketch-size 1000 --ordered-kmer-size 14 --min-olap-length 2000 --repeat-idf-scale 50" mhapMemory=60g mhapBlockSize=500 ovlMerDistinct=0.975 -pacbio-raw /data/input/DRR213641.fastq > RKN.out 2> RKN.err
