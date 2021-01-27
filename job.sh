22ab405bb345b1d320d41086b08de83380362af5
brentmcconnell <emconne@gmail.com>
2021-01-27 10:28:10 -0500

    changed job to ecoli

--- job.sh
+++ job.sh
@@ -2,25 +2,32 @@
 
 source activate denovo_asm
 
-# check to see if file already exists and delete
-if [ -f /data/input/DRR213641 ]; then
-  echo "Removing existing file"
-  rm /data/input/DRR213641
-fi
+curl -L -o /data/input/pacbio.fastq http://gembox.cbcb.umd.edu/mhap/raw/ecoli_p6_25x.filtered.fastq
 
-# check to see if file already exists and delete
-if [ -f /data/input/DRR213641.fastq ]; then
-  echo "Removing existing fastq file"
-  rm /data/input/DRR213641.fastq
-fi
+time canu \
+ -p ecoli -d /data/assembly/ecoli-pacbio \
+ genomeSize=4.8m \
+ -pacbio /data/input/pacbio.fastq | tee /data/input/ecoli.log
 
-wget -nv https://sra-download.ncbi.nlm.nih.gov/traces/dra4/DRR/000208/DRR213641 -P /data/input
+# # check to see if file already exists and delete
+# if [ -f /data/input/DRR213641 ]; then
+#   echo "Removing existing file"
+#   rm /data/input/DRR213641
+# fi
 
-fasterq-dump /data/input/DRR213641 -O /data/input
+# # check to see if file already exists and delete
+# if [ -f /data/input/DRR213641.fastq ]; then
+#   echo "Removing existing fastq file"
+#   rm /data/input/DRR213641.fastq
+# fi
 
-time canu -p RKN -d /data/assembly/RKN_canu \
-  genomeSize=0.2g corMhapFilterThreshold=0.0000000002 \
-  mhapMemory=60g mhapBlockSize=500 \
-  ovlMerDistinct=0.975 \
-  corMhapOptions="--threshold 0.80 --num-hashes 512 --num-min-matches 3 --ordered-sketch-size 1000 --ordered-kmer-size 14 --min-olap-length 2000 --repeat-idf-scale 50" \
-  -pacbio-raw /data/input/DRR213641.fastq > RKN.out 2> RKN.err
+# wget -nv https://sra-download.ncbi.nlm.nih.gov/traces/dra4/DRR/000208/DRR213641 -P /data/input
+
+# fasterq-dump /data/input/DRR213641 -O /data/input
+
+# time canu -p RKN -d /data/assembly/RKN_canu \
+#   genomeSize=0.2g corMhapFilterThreshold=0.0000000002 \
+#   mhapMemory=60g mhapBlockSize=500 \
+#   ovlMerDistinct=0.975 \
+#   corMhapOptions="--threshold 0.80 --num-hashes 512 --num-min-matches 3 --ordered-sketch-size 1000 --ordered-kmer-size 14 --min-olap-length 2000 --repeat-idf-scale 50" \
+#   -pacbio-raw /data/input/DRR213641.fastq > RKN.out 2> RKN.err
