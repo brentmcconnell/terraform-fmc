b6ee2ea76106b0ff8a1f671b959ea6f600f39bbe
brentmcconnell <emconne@gmail.com>
2021-01-29 16:43:22 -0500

    process all fastq files found

--- job.sh
+++ job.sh
@@ -12,10 +12,10 @@ ls -la
 FASTQ_FILES=$(find . -name '*.fastq' -not -type d | tr '\n' ' ')
 echo $FASTQ_FILES
 
-time canu \
- -p ecoli -d /data/assembly/RUN \
- genomeSize=4.8m \
- -pacbio $FASTQ_FILES | tee /data/input/run.log
+# time canu \
+#  -p ecoli -d /data/assembly/RUN \
+#  genomeSize=4.8m \
+#  -pacbio $FASTQ_FILES | tee /data/input/run.log
 
 # # check to see if file already exists and delete
 # if [ -f /data/input/DRR213641 ]; then
@@ -33,9 +33,9 @@ time canu \
 
 # fasterq-dump /data/input/DRR213641 -O /data/input
 
-# time canu -p RKN -d /data/assembly/RKN_canu \
-#   genomeSize=0.2g corMhapFilterThreshold=0.0000000002 \
-#   mhapMemory=60g mhapBlockSize=500 \
-#   ovlMerDistinct=0.975 \
-#   corMhapOptions="--threshold 0.80 --num-hashes 512 --num-min-matches 3 --ordered-sketch-size 1000 --ordered-kmer-size 14 --min-olap-length 2000 --repeat-idf-scale 50" \
-#   -pacbio-raw /data/input/DRR213641.fastq > RKN.out 2> RKN.err
+time canu -p RKN -d /data/assembly/RKN_canu \
+  genomeSize=0.2g corMhapFilterThreshold=0.0000000002 \
+  mhapMemory=60g mhapBlockSize=500 \
+  ovlMerDistinct=0.975 \
+  corMhapOptions="--threshold 0.80 --num-hashes 512 --num-min-matches 3 --ordered-sketch-size 1000 --ordered-kmer-size 14 --min-olap-length 2000 --repeat-idf-scale 50" \
+  -pacbio-raw $FASTQ_FILES | tee /data/input/run.log
