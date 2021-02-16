0d31b2235aa238ade4f8ce4244774dbfe557e374
brentmcconnell <emconne@gmail.com>
2021-02-16 11:17:11 -0500

    refactored

--- iac/backend.tf
+++ iac/backend.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name    = "tfstateacct0693"
     container_name          = "tfcont-0693"
     key                     = "terraform.tfstate"
-    access_key              = "C+42I7MBDtPkTnt2iYJ5eMVFafMrpHhAgd4LZsgRGInlAFIIJOShzM5qfHfXqYy6ns6RjVUI7a+cF0+wiJ7bkQ=="
   }
 }
